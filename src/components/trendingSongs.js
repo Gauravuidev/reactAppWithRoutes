133d81980717e81ab89e06918feb9b6c6a6b69d3
Gaurav Kukreja <gaurav.kukreja@gaana.com>
2018-06-04 13:00:40 +0530

    changes for songs page

--- src/components/trendingSongs.js
+++ src/components/trendingSongs.js
@@ -68,14 +68,14 @@ export class Trending extends Component {
     });
     const data = this.state.data;
     const TData =   Object.keys(data).map(key =>
-    <div data-seo={data[key].seokey} onClick={this.getIds.bind(this)}>
+    <Link to={'/Songs'} data-seo={data[key].seokey} onClick={this.getIds.bind(this)}>
       <div className="c_item list   loaded" id={"trackrow" + data[key].entity_id}>
-                <span id="parent-row-song23594885" className={"none parentnode sourcelist_"+ data[key].entity_id} data-id={data[key].entity_id} data-title={data[key].name}>
+                <span id={"parent-row-song" + data[key].entity_id} className={"none parentnode sourcelist_"+ data[key].entity_id} data-id={data[key].entity_id} data-title={data[key].name}>
                 </span>
                 <img className="img opacity" data-value={"song" + data[key].entity_id} data-type="touch-play" title={data[key].name} alt={data[key].name} src={data[key].artwork} data-src={data[key].artwork} />
                 <span data-value={"song" + data[key].entity_id} data-type="touch-play" title={data[key].name} className="c_song pjax">{data[key].name}</span>
       </div>
-    </div>
+    </Link>
     )
     return (
           <Carousel showArrows={false} showIndicators={false} showStatus={false} showThumbs={false} centerMode centerSlidePercentage={40} swipeScrollTolerance={5}>
