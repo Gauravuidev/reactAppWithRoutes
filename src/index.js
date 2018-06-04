3abbda0e10b920cceb64182508f156cd7637b58a
vivek9716 <nice.vivek25@gmail.com>
2018-06-04 16:22:04 +0530

    Remove blank lines

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-
-
-
-
 //import CenterContent from './components/CenterContent';
 
 render((
